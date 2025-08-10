00a32ebba8dc1d2cac895d95d421872bd1681598
leonfoeck <leonfoeck@gmail.com>
2025-08-11 01:10:51 +0200

    Added Job builder site

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request, { Response } from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -15,11 +15,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', async () => {
+    await request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect((res) => {
+      .expect((res: Response) => {
         expect(res.body).toHaveProperty('message', 'Job Matcher API');
       });
   });
